f46c71dc122b6059638cf3203c21aa07e5b487e5
AgawaToshiki <t.agawa1128ht@gmail.com>
2025-01-08 23:09:02 +0900

    環境変数名の変更

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const response = await fetch("https://api.clashofclans.com/v1/clans/%23G0UUYP82", {
     method: "GET",
     headers: {
-      'Authorization': `Bearer ${process.env.API_TOKEN}`,
+      'Authorization': `Bearer ${process.env.API_KEY}`,
       'Content-Type': 'application/json'
     }
   })
