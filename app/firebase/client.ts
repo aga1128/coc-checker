758e5bdcbb2894e7f4ac5790308f08215325aedf
AgawaToshiki <t.agawa1128ht@gmail.com>
2025-01-21 17:42:18 +0900

    ミドルウェアで認証チェックできるように変更

--- app/firebase/client.ts
+++ app/firebase/client.ts
@@ -13,7 +13,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const analytics = getAnalytics(app);
--- auth.ts
+++ auth.ts
@@ -1,15 +1,10 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google"
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { adminDB } from "./app/firebase/server";
+import authConfig from "./auth.config";
  
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    })
-  ],
   adapter: FirestoreAdapter(adminDB),
+  ...authConfig
 })
