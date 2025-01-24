00cc68dc23497c2b57669ad4f15071176fc46c7a
Agawa <t.agawa1128ht@gmail.com>
2025-01-24 14:30:43 +0900

    軽微修正

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,7 @@ import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
  
 export default { providers: [
-  Google( {
+  Google({
     clientId: process.env.AUTH_GOOGLE_ID,
     clientSecret: process.env.AUTH_GOOGLE_SECRET
   })
