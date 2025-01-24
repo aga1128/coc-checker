fa7f3526d4dc2f98134cbecad1891d4115d13e79
Agawa <t.agawa1128ht@gmail.com>
2025-01-24 14:48:13 +0900

    sessionデータの表示

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,14 @@
+import { auth } from "@/auth";
 import SearchUser from "../components/SearchUser";
 
 export default async function DashBoard() {
 
+  const session = await auth();
+  if (!session?.user) return null
+
   return (
     <>
+      {session.user.name}
       <SearchUser />
     </>
   );
