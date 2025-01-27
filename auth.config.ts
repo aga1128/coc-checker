b2277d8f0362d39119c0c19af3fcd53aa5171615
AgawaToshiki <t.agawa1128ht@gmail.com>
2025-01-28 01:25:30 +0900

    configの変更

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,15 @@
 import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
  
-export default { providers: [
-  Google({
-    clientId: process.env.AUTH_GOOGLE_ID,
-    clientSecret: process.env.AUTH_GOOGLE_SECRET
-  })
-] } satisfies NextAuthConfig
+export default {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET
+    })
+  ],
+  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: 'jwt'
+  }
+} satisfies NextAuthConfig
