919deadc4eb998e081db461751a45b11bd88a874
Agawa <t.agawa1128ht@gmail.com>
2025-02-28 09:14:06 +0900

    header修正

--- app/components/layouts/Navigation.tsx
+++ app/components/layouts/Navigation.tsx
@@ -3,11 +3,6 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
-type Props = {
-  isAdmin: boolean;
-  id: string;
-}
-
 const Navigation = () => {
 
   const pathname = usePathname();
@@ -17,15 +12,11 @@ const Navigation = () => {
       href: "/",
       pathname: "/",
       title: "ダッシュボード",
-      icon: "dashboard",
-      showFlag: true,
     },
     {
       href: "/setting",
       pathname: "/setting",
       title: "設定",
-      icon: "setting",
-      showFlag: true
     },
   ]
  
@@ -33,16 +24,14 @@ const Navigation = () => {
     <nav>
       <ul className="flex gap-3">
         {lists.map((list) => (
-          list.showFlag && (
-            <li key={list.href}>
-              <Link 
-                href={list.href}
-                className={`${list.pathname === pathname && "bg-red-300 font-semibold"} flex items-center gap-4 rounded-sm px-4 py-2 transition duration-100 ease-in-out cursor-pointer hover:bg-red-400 active:bg-red-300`}
-              >
-                {list.title}
-              </Link>
-            </li>
-          )
+          <li key={list.href}>
+            <Link 
+              href={list.href}
+              className={`${list.pathname === pathname && "bg-red-300 font-semibold"} flex items-center gap-4 rounded-sm px-4 py-2 transition duration-100 ease-in-out cursor-pointer hover:bg-red-400 active:bg-red-300`}
+            >
+              {list.title}
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
